7b1e21beb372eb188f0d1d07057c304acdd032ba
Abubakar Abid <islamrealm@gmail.com>
2026-01-13 12:36:05 -0800

    changes

--- test_app.py
+++ test_app.py
@@ -2,8 +2,8 @@ from toolsets import Server, Toolset
 
 t = Toolset()
 
-t.add(Server("prithivMLmods/Qwen-Image-Edit-2511-LoRAs-Fast")).add(
-    Server("zerogpu-aoti/wan2-2-fp8da-aoti")
-).add(Server("https://linoyts-qwen-image-edit-angles.hf.space/gradio_api/mcp/"))
+t.add(Server("prithivMLmods/Qwen-Image-Edit-2511-LoRAs-Fast"))
+t.add(Server("zerogpu-aoti/wan2-2-fp8da-aoti"))
+t.add(Server("https://linoyts-qwen-image-edit-angles.hf.space/gradio_api/mcp/"))
 
 t.launch()
--- toolsets/toolset.py
+++ toolsets/toolset.py
@@ -1,4 +1,6 @@
-from typing import List, Optional
+from typing import List
+
+import gradio as gr
 
 from .toolset_element import ToolsetElement
 
@@ -6,12 +8,28 @@ from .toolset_element import ToolsetElement
 class Toolset:
     def __init__(self):
         self._elements: List[ToolsetElement] = []
+        self._tool_data: Dict[str, Dict[str, Any]] = None
 
     def add(self, element: ToolsetElement) -> "Toolset":
         self._elements.append(element)
         return self
 
-    def launch(
-        self, port: int = 7860, share: bool = False, server_name: Optional[str] = None
-    ):
-        pass
+    def _get_tool_data(self) -> Dict[str, Dict[str, Any]]:
+        if self._tool_data is not None:
+            return
+        for element in self._elements:
+            tools = element.get_tools()
+            for tool in tools:
+                self._tool_data[tool.pop("name")] = tool
+
+    def launch(self):
+        self._get_tool_data()
+
+        with gr.Blocks() as demo:
+            gr.Markdown(f"This Toolset contains {len(self._tool_data)} tools:")
+            for tool_name, tool_data in self._tool_data.items():
+                gr.Markdown(f"### {tool_name}")
+                gr.Markdown(f"**Description:** {tool_data['description']}")
+                gr.Markdown(f"**Input Schema:** {tool_data['inputSchema']}")
+
+        demo.launch()
