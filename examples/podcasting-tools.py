39c6400278242b9fe39354865ab44c840b6ecf6f
Abubakar Abid <islamrealm@gmail.com>
2026-01-14 10:43:35 -0800

    changes

--- examples/podcasting-tools.py
+++ examples/podcasting-tools.py
@@ -2,10 +2,10 @@ from toolsets import Server, Toolset
 
 t = Toolset("Podcasting Tools")
 
-t.add(Server("MohamedRashad/Audio-Separator"))
-# t.add(Server("Tonic/audiocraft"))
-# t.add(Server("hf-audio/whisper-large-v3"))
-# t.add(Server("ResembleAI/Chatterbox"))
-# t.add(Server("maya-research/maya1"))
+# t.add(Server("MohamedRashad/Audio-Separator"))
+t.add(Server("Tonic/audiocraft"))
+t.add(Server("hf-audio/whisper-large-v3"))
+t.add(Server("ResembleAI/Chatterbox"))
+t.add(Server("maya-research/maya1"))
 
 t.launch()
